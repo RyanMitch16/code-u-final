9104f34fc569031daed62eacc970293feb155855
RyanMitch16 <ryanmitch16@gmail.com>
2015-07-20 16:15:26 -0500

    Added documentation for app engine

---
+++ appengine/README.md
@@ -0,0 +1,18 @@
+# code-u-final
+
+AppEngine API Documentation
+
+#How to create new users
+http://code-u-final.appspot.com/user/create?email=[EMAIL_ADDRESS]
+
+Returns the key that represents the user on sucessful completion (Status code 201)
+
+#How to create a new list
+http://code-u-final.appspot.com/list/create?user_key=[USER_KEY]&list_name=[LIST_NAME]
+
+Returns the key that represents the list on sucessful completion (Status code 201)
+
+
+
+
+    
--- appengine/code-u-final/main.py
+++ appengine/code-u-final/main.py
@@ -23,9 +23,11 @@ from google.appengine.ext import ndb
 from user import User
 from itemlist import ItemList
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+
 
 class UserCreateHandler(webapp2.RequestHandler):
     def get(self):
@@ -72,11 +74,11 @@ class ItemListCreateHandler(webapp2.RequestHandler):
         #Checks if user exists
         user_key = ndb.Key(urlsafe=self.request.get('user_key'))
         user = user_key.get()
-        if (user == null){
+        if (user == None):
             self.response.set_status(400)
             self.response.write("The user does not exist")
             return
-        }
+        
         
         #Set the list to empty
         list_content = {
@@ -93,18 +95,85 @@ class ItemListCreateHandler(webapp2.RequestHandler):
         user.item_lists = json.encode(user_item_lists)
         user.put()
 
-        #Respond with a success
+        #Respond with the list key
         self.response.set_status(201)
-        self.response.write(user_item_lists)
+        self.response.write(item_list_key)
 
-class ItemListItemHandler(webapp2.RequestHandler):
+
+class ItemListEditHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        opcode = self.request.get('opcode')
+        
+        #Checks if user exists
+        user_key = ndb.Key(urlsafe=self.request.get('user_key'))
+        user = user_key.get()
+        if (user == None):
+            self.response.set_status(400)
+            self.response.write("The user does not exist")
+            return
+        
+        #Checks if the list exists
+        list_key_str = self.request.get('list_key')
+        list_key = ndb.Key(urlsafe=list_key_str)
+        list = list_key.get()
+        if (list == None):
+            self.response.set_status(400)
+            self.response.write("The list does not exist")
+            return
+        
+        #Checks if the user is allowed to edit the list
+        allowed = False
+        user_item_lists = json.decode(user.item_lists)
+        for item_list in user_item_lists["itemLists"]:
+            if (item_list == list_key_str):
+                allowed = True
+                break
+        if (allowed == False):
+            self.response.set_status(403)
+            self.response.write("User not allowed to edit list")
+            return
+
+        if (opcode == "add"):
+            #Check if item name provided
+            item_name = self.request.get('item_name')
+            if (item_name == ""):
+                self.response.set_status(400)
+                self.response.write("Item name not provided")
+                return
+        
+            #Check if item quantity provided
+            item_quantity = self.request.get('item_quantity')
+            if (item_quantity == ""):
+                self.response.set_status(400)
+                self.response.write("Item Quantity not provided")
+                return
+            
+            #Construct the new item
+            item = {
+                "name" : item_name,
+                "item-quantity" : item_quantity
+            }
+            
+            #Add the item to the list
+            content = json.decode(list.content)
+            content["items"].append(item)
+            list.content = json.encode(content)
+            list.put()
+            
+            self.response.set_status(201)
+            self.response.write(content)
+
+        elif (opcode == "delete"):
+            self.response.set_status(201)
+            self.response.write("Deleted")
+        else:
+            self.response.set_status(400)
+            self.response.write("Invalid operation")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/user/create', UserCreateHandler),
-    ('/list/create',ItemListCreateHandler)
-    ('/list/item',ItemListCreateHandler)
+    ('/list/create',ItemListCreateHandler),
+    ('/list/edit',ItemListEditHandler)
 ], debug=True)
